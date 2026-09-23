90c2245a546434271f2c2e6e4f154da09b3c56b9
BusisaMdali <kgaritjejan@gmail.com>
2026-09-23 09:35:12 +0200

    tip-splitter.js

--- Exercises/Day01_Web_and_JavaScript_Essentials/exercise-1-tip-splitter/tip-splitter.js
+++ Exercises/Day01_Web_and_JavaScript_Essentials/exercise-1-tip-splitter/tip-splitter.js
@@ -3,6 +3,7 @@
 
 function calculateTip(bill, percent) {
   const tip = bill * (percent / 100);
+  return tip;
 }
 
 function totalWithTip(bill, percent) {
@@ -12,7 +13,7 @@ function totalWithTip(bill, percent) {
 function describeLunch(lunch) {
   const total = totalWithTip(lunch.bill, lunch.percent);
 
-  if (lunch.people = 1) {
+  if (lunch.people === 1) {
     return lunch.place + ": total R" + total + " (no need to split)";
   }
 
@@ -31,7 +32,7 @@ console.log("Team lunch tip splitter");
 console.log("-----------------------");
 
 let splitCount = 0;
-for (let i = 0; i <= lunches.length; i++) {
+for (let i = 0; i < lunches.length; i++) {
   console.log(describeLunch(lunches[i]));
   if (lunches[i].people > 1) {
     splitCount++;
